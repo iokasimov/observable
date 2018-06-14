72cfc317b253ac17f695252f9ee707b03cc5256d
Murat Kasimov <iokasimov.m@gmail.com>
2018-06-14 18:14:06 +0300

    Add @dispatch@, @obs@, @subscribe@ and @notify@

--- Control/Observable.hs
+++ Control/Observable.hs
@@ -1,5 +1,4 @@
-module Control.Observable
-	(Observable (..), dispatch, subscribe, notify, obs) where
+module Control.Observable (Observable, dispatch, subscribe, notify, obs) where
 
 import Control.Monad (forever)
 import Control.Monad.Trans.Cont (ContT (..))
@@ -12,11 +11,11 @@ type Observable f a b = ContT b (Mock b f) a
 dispatch :: ContT b f a -> Observable f a b
 dispatch f = ContT $ \h -> Mock $ runContT f (mock . h)
 
+obs :: Monad f => f a -> Observable f a b
+obs action = dispatch (lift action)
+
 subscribe :: Applicative f => Observable f a b -> (a -> f b) -> f b
 subscribe r f = forever $ mock $ runContT r (Mock . f)
 
 notify :: Observable f a b -> (a -> f b) -> f b
 notify r f = mock $ runContT r (Mock . f)
-
-obs :: Monad f => f a -> Observable f a b
-obs action = dispatch (lift action)
