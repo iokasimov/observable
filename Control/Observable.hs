bc640eed8752aa1bcc881340d56ecae610da7f93
Murat Kasimov <iokasimov.m@gmail.com>
2018-07-08 20:30:43 +0300

    Rename Mock to Capture

--- Control/Observable.hs
+++ Control/Observable.hs
@@ -6,14 +6,14 @@ import Control.Monad.Trans.Cont (ContT (..))
 import Control.Monad.Trans.Class (lift)
 import Data.Function (($), (.))
 
-newtype Mock r f a = Mock { mock :: f r }
+newtype Capture r f a = Capture { captured :: f r }
 
--- | Mock used here as delimiter
-type Observable f a r = ContT r (Mock r f) a
+-- | Capture used here as delimiter
+type Observable f a r = ContT r (Capture r f) a
 
 -- | Make continuation to be observable
 dispatch :: ContT r f a -> Observable f a r
-dispatch f = ContT $ \h -> Mock $ runContT f (mock . h)
+dispatch f = ContT $ \h -> Capture $ runContT f (captured . h)
 
 -- | Make monadic action to be observable
 obs :: Monad f => f a -> Observable f a r
@@ -21,8 +21,8 @@ obs action = dispatch $ lift action
 
 -- | Listen all event from action, forever
 subscribe :: Applicative f => Observable f a r -> (a -> f r) -> f r
-subscribe r f = forever $ mock $ runContT r (Mock . f)
+subscribe r f = forever $ captured $ runContT r (Capture . f)
 
 -- | Listen only first event, just once
 notify :: Observable f a r -> (a -> f r) -> f r
-notify r f = mock $ runContT r (Mock . f)
+notify r f = captured $ runContT r (Capture . f)
